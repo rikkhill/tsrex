236905ac547f544b3af514be6268d2548bb5fc44
Rikk Hill <rikk@unashamedly.co.uk>
2019-06-16 21:01:23 +0100

    Added test framework

---
+++ tsrex_core/test/test_smoke.cpp
@@ -0,0 +1,9 @@
+//
+// Created by rikk on 16/06/19.
+//
+
+#include "catch.hpp"
+
+TEST_CASE("Test in another file", "[junk_tests") {
+    REQUIRE(true);
+}
---
+++ tsrex_core/test/testrunner.cpp
@@ -0,0 +1,12 @@
+//
+// Created by rikk on 16/06/19.
+//
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("True things are true", "[junk_tests]") {
+
+    REQUIRE(true);
+
+}
